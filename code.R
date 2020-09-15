364dd35715d26f5e45837f3b1e8d9ceae30ffea6
Nathan Fischer <nathanlfischer@gmail.com>
2020-09-15 06:54:37 -0500

    First upload

---
+++ code.R
@@ -0,0 +1,7 @@
+#Install and load required packages
+if(!require(tidyverse)) install.packages("tidyverse", repos = "http://cran.us.r-project.org")
+if(!require(caret)) install.packages("caret", repos = "http://cran.us.r-project.org")
+
+library(tidyverse)
+library(caret)
+
